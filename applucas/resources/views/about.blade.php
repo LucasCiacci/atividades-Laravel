915338e6e2af1f20db69757abaf251662c3b237a
Lucas Ciacci <lucas.ciacci@alunos.unis.edu.br>
2024-08-21 20:53:22 -0300

    Subindo as duas atividades

---
+++ applucas/resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@entends('layouts.app')
+
+@section('content')
+    <h1>About Us</h1>
+    <p>This page contains information about us.</p>
+@endsection    
---
+++ applucas/resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@entends('layouts.app')
+
+@section('content')
+    <h1>Contact Us</h1>
+    <p>This page contains contact information.</p>
+@endsection
